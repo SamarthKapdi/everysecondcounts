f0409f942cfe73fbcbeabed0278e569e686e8ae3
Samarth Kapdi <kapdisamarth26445@gmail.com>
2026-09-16 10:34:44 +0530

    docs: add description to ambulance routes

--- server/src/routes/ambulanceRoutes.js
+++ server/src/routes/ambulanceRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ambulanceController = require('../controllers/ambulanceController');
 
+// GET /api/ambulances - Get list of all ambulances
 router.get('/', ambulanceController.getAllAmbulances);
 
 module.exports = router;
